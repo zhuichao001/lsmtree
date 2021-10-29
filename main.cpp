a8c5006af289b2dd52710b93fa284f0ada68f118
zhuichao001 <zhuichao001@163.com>
2021-10-30 05:29:05 +0800

    add test case

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 #include "lsmtree.h"
 
-
-int main(){
+void test0(){
     lsmtree db;
     db.open("./data");
-    db.put("a","b");
+    for(int i=0; i<100000; ++i){
+        char k[128];
+        char v[128];
+        sprintf(k, "key_%d\0", i);
+        sprintf(v, "val_%d\0", i);
+        db.put(k, v);
+    }
 
     std::string val;
-    db.get("a",  val);
-    std::cout<< "val:" << val <<std::endl;
+    for(int i=0; i<100000; ++i){
+        char k[128];
+        char v[128];
+        sprintf(k, "key_%d\0", i);
+        sprintf(v, "val_%d\0", i);
+        db.get(k,  val);
+        if(std::string(k)==val){
+            std::cout<< "yes same:" <<k <<std::endl;
+        }else{
+            std::cout<< "not same:" <<k <<std::endl;
+        }
+    }
+}
+
+int main(){
+    test0();
     return 0;
 }
